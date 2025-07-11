a9eed0a01cb9c3f7128f0ee5d893af5b4820d1b4
Federica Adobbati <f.adobbati@campus.unimib.it>
2025-07-11 18:21:17 +0200

    more flexible dataset creation

--- make_pt_ds.py
+++ make_pt_ds.py
@@ -9,30 +9,26 @@ from typing import List, Dict
 import json
 
 
-def get_xy_single_var(data_path:str, cms2ogs_map:Dict[str, str], cms_name: str, is_test:bool, is_surface:bool):
+def get_xy_single_var(data_path:str, cms2ogs_map:Dict[str, str], cms_name: str, is_test:bool):
     '''
         Returns the input and target values corresponding to a given variable in the data folder.
 
         Args:
-            data_path (str): path to the data directory, there must be a subfolder called 'original' if we want
-            3D data, 'surface' for 2D data
+            data_path (str): path to the data directory
             cms2ogs_map: dictionary associating Copernicus Marine names to CADEAU names
             cms_name (str): variable name in the Copernicus Marine notation.
             is_test (bool): True if we are considering test dataset, False otherwise
-            is_surface (bool): True if we want to consider only surface, False otherwise
         Returns:
             x_arr (numpy array): input of the DL model for the given variable
             y_arr (numpy array): target of the DL model for the given variable
     '''
     ogs_name = cms2ogs_map[cms_name]
-    sub_folder = "surface" if is_surface else "original"
 
-    main_folder_cms = "nc_iCMS_test" if is_test else "nc_iCMS"
-    main_folder_ogs = "nc_OGS_test" if is_test else "nc_OGS"
+    main_folder_cms = "nc_iCMS_test" if is_test else "iCMS_nc"
+    main_folder_ogs = "nc_OGS_test" if is_test else "NARF_nc"
 
-
-    cms_path = os.path.join(data_path, sub_folder, main_folder_cms, cms_name)
-    ogs_path = os.path.join(data_path, sub_folder, main_folder_ogs, ogs_name)
+    cms_path = os.path.join(data_path, main_folder_cms, cms_name)
+    ogs_path = os.path.join(data_path, main_folder_ogs, ogs_name)
 
     x_list = []; y_list=[]
 
@@ -41,7 +37,7 @@ def get_xy_single_var(data_path:str, cms2ogs_map:Dict[str, str], cms_name: str, 
 
     ds_type = "test" if is_test else "train"
 
-    with alive_bar(0, title=f"Processing CMS {ds_type} data for {cms_name}...") as bar:
+    with alive_bar(len(cms_filenames), title=f"Processing CMS {ds_type} data for {cms_name}...") as bar:
         for filename in cms_filenames:
 
             file_path = os.path.join(cms_path, filename)
@@ -49,7 +45,7 @@ def get_xy_single_var(data_path:str, cms2ogs_map:Dict[str, str], cms_name: str, 
             x_list.append(file_ds[cms_name][:].data)
             bar()
 
-    with alive_bar(0, title=f"Processing OGS {ds_type} data for {ogs_name}...") as bar:
+    with alive_bar(len(ogs_filenames), title=f"Processing OGS {ds_type} data for {ogs_name}...") as bar:
         for filename in ogs_filenames:
 
             file_path: str = os.path.join(ogs_path, filename)
@@ -70,7 +66,7 @@ def get_river_vector(data_path:str, is_test:bool):
 
     ds_type = "test" if is_test else "train"
 
-    with alive_bar(0, title=f"Processing river {ds_type} data ...") as bar:
+    with alive_bar(len(cms_filenames), title=f"Processing river {ds_type} data ...") as bar:
         for filename in cms_filenames:
             file_path = os.path.join(river_path, filename)
             x=np.loadtxt(file_path)
@@ -79,18 +75,22 @@ def get_river_vector(data_path:str, is_test:bool):
     return np.array(x_list)
 
 
-def make_rivers_dataset(data_path:str):
+def make_rivers_dataset(data_path:str, train_only: bool = False, test_only:bool = False):
     """Construct river training and test torch dataset.
     """
 
-    rivers_train = get_river_vector(data_path, is_test=False)
-    rivers_test = get_river_vector(data_path, is_test=True)
-    rivers_train_save_path = os.path.join(data_path, "rivers", "rivers_train.pt")
-    rivers_test_save_path = os.path.join(data_path, "rivers", "rivers_test.pt")
-    print("Saving the pytorch river datasets...")
-    torch.save(torch.Tensor(rivers_train), rivers_train_save_path)
-    torch.save(torch.Tensor(rivers_test), rivers_test_save_path)
-    print("Saved!")
+    if not test_only:
+        rivers_train = get_river_vector(data_path, is_test=False)
+        rivers_train_save_path = os.path.join(data_path, "rivers", "rivers_train.pt")
+        print("Saving the pytorch river datasets...")
+        torch.save(torch.Tensor(rivers_train), rivers_train_save_path)
+        print("Saved!")
+    if not train_only:
+        rivers_test = get_river_vector(data_path, is_test=True)
+        rivers_test_save_path = os.path.join(data_path, "rivers", "rivers_test.pt")
+        print("Saving the pytorch river datasets...")
+        torch.save(torch.Tensor(rivers_test), rivers_test_save_path)
+        print("Saved!")
 
 
 def get_mean_std(ds:np.array, mask:np.array):
@@ -122,7 +122,7 @@ def normalize(ds:np.array, means:np.array, stds:np.array, mask:np.array):
     return normalized_ds.data
 
 
-def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, str], is_surface:bool = False, stat:bool = True):
+def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, str], stat:bool = True, train_only: bool = False, test_only:bool = False):
     '''
         Saves the Pytorch dataset corresponding to a set of given variables in the data folder.
 
@@ -132,8 +132,7 @@ def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, st
             of the datasets.
             var_list (List[str]): list of variable names in the Copernicus Marine notation.
             cms2ogs_map: dictionary associating to Copernicus Marine names CADEAU names
-            is_surface (opt, bool): True if we want to consider 2D data, False if data are 3D. Default is 3D
-            is_surface (opt, bool): True (default) if we have already computed means and standard deviations for the variables, False if we need to do it
+            stat (opt, bool): True (default) if we have already computed means and standard deviations for the variables, False if we need to do it
     '''
 
     cms_im_train = []
@@ -147,12 +146,14 @@ def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, st
     y_stds = []
 
     for var in var_list:
-        varx_list, vary_list = get_xy_single_var(data_path, cms2ogs_map, var, False, is_surface)
-        cms_im_train.append(varx_list)
-        ogs_im_train.append(vary_list)
-        varx_list, vary_list = get_xy_single_var(data_path, cms2ogs_map, var, True, is_surface)
-        cms_im_test.append(varx_list)
-        ogs_im_test.append(vary_list)
+        if not test_only:
+            varx_list, vary_list = get_xy_single_var(data_path, cms2ogs_map, var, False)
+            cms_im_train.append(varx_list)
+            ogs_im_train.append(vary_list)
+        if not train_only:
+            varx_list, vary_list = get_xy_single_var(data_path, cms2ogs_map, var, True)
+            cms_im_test.append(varx_list)
+            ogs_im_test.append(vary_list)
         if stat:
             with open(f'{data_path}/statistics/cms/stat_cms_{var}.txt', 'r') as file:
                 line_elements = []
@@ -195,12 +196,15 @@ def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, st
         x_means, x_stds = get_mean_std(x_full, mask)
         y_means, y_stds = get_mean_std(y_full, mask)
 
-    x_train = normalize(x_train, x_means, x_stds, mask)
-    x_test = normalize(x_test, x_means, x_stds, mask)
-    y_train = normalize(y_train, y_means, y_stds, mask)
-    y_test = normalize(y_test, y_means, y_stds, mask)
-    train_torch_ds = TensorDataset(torch.Tensor(x_train), torch.Tensor(y_train))
-    test_torch_ds = TensorDataset(torch.Tensor(x_test), torch.Tensor(y_test))
+    if not test_only:
+        x_train = normalize(x_train, x_means, x_stds, mask)
+        y_train = normalize(y_train, y_means, y_stds, mask)
+        train_torch_ds = TensorDataset(torch.Tensor(x_train), torch.Tensor(y_train))
+    if not train_only:
+        x_test = normalize(x_test, x_means, x_stds, mask)
+        y_test = normalize(y_test, y_means, y_stds, mask)
+        test_torch_ds = TensorDataset(torch.Tensor(x_test), torch.Tensor(y_test))
+
     if len(var_list) == len(cms2ogs_map):
         name = "all_"
     else:
@@ -208,14 +212,18 @@ def make_var_dataset(data_path:str, var_list:List[str], cms2ogs_map:Dict[str, st
         for var in var_list:
             name = name + f"{var}_"
 
-    dest_path = os.path.join(data_path, "surface" if is_surface else "original")
+    dest_path = data_path #os.path.join(data_path, "surface" if is_surface else "original")
 
-    train_save_path = os.path.join(dest_path, name + "train_dataset.pt")
-    test_save_path =  os.path.join(dest_path, name + "test_dataset.pt")
-    print("Saving the pytorch datasets...")
-    torch.save(train_torch_ds, train_save_path)
-    torch.save(test_torch_ds, test_save_path)
-    print("Saved!")
+    if not test_only:
+        train_save_path = os.path.join(dest_path, name + "train_dataset.pt")
+        print("Saving the pytorch datasets...")
+        torch.save(train_torch_ds, train_save_path)
+        print("Saved!")
+    if not train_only:
+        test_save_path =  os.path.join(dest_path, name + "test_dataset.pt")
+        print("Saving the pytorch datasets...")
+        torch.save(test_torch_ds, test_save_path)
+        print("Saved!")
 
 
 
@@ -251,9 +259,10 @@ if __name__== "__main__":
     i = 1
     var_list = []
     data_path = None
-    is_surface = False
     rivers = False
     read_stat = False
+    train_only = False
+    test_only = False 
 
     while i < len(sys.argv):
         if sys.argv[i] == "-dp":
@@ -278,28 +287,31 @@ if __name__== "__main__":
                 i+= 1
         elif sys.argv[i] == "-r":
             rivers = True; i+=1
+        elif sys.argv[i] == "-train_only":
+            train_only = True; i+= 1
+        elif sys.argv[i] == "-test_only":
+            test_only = True; i+= 1
         elif sys.argv[i] == "-h" or sys.argv[i] == "--help":
             help()
             i+=1
-        elif sys.argv[i] == "-s":
-            is_surface = True; i+=1
         else:
             i+=1
 
     if data_path is None:
         help()
-    if v == []:
-        help()
+    if var_list == []:
+        print("[WARNING] No variables provided with -v. No variable dataset will be created.")
 
-    map_path = os.path.join(data_path, "cms2ogs.json") # dictionary that assign CADEAU name of variables to CMS names
+    map_path = os.path.join(data_path, "cms2ogs.json") # dictionary that assign CADEAU name of variables to Copernicus Marine names
     with open(map_path, 'r') as f:
         cms2ogs_map = json.load(f)
     if var_list == "all":
-        var_list = list(cms2ogs_map.keys())
+        var_list = list(cms2ogs_map.keys())  # ??
 
     print(f"[make_dataset for variables {var_list}] Starting execution")
     if var_list != []:
-        make_var_dataset(data_path, var_list, cms2ogs_map, is_surface, read_stat)
+        make_var_dataset(data_path, var_list, cms2ogs_map, read_stat, train_only, test_only)
     if rivers:
-        make_rivers_dataset(data_path)
+        make_rivers_dataset(data_path, train_only, test_only)
     print(f"[make_dataset for variable {var_list}] Ending execution")
+    
